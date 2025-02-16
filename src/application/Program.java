0863dc49432f9c6527e30217ea9cd723554dac2a
heytulio <tuliobackup1@outlook.com>
2025-02-16 16:43:26 -0300

    logica da forca implementada

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Scanner;
+import forca.Maestro;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Maestro forca = new Maestro();
+        
+        forca.adicionarPalavra("mochila", "criança", "diabo", "rede", "pá", "fone", "LUZ", "Medo");
+
+        System.out.println("Bem-vindo ao jogo da Forca!");
+        System.out.println("Tente adivinhar a palavra uma letra por vez.");
+        System.out.println("Você tem 5 vidas. Boa sorte!\n");
+
+        boolean jogarNovamente;
+        
+        do {
+            forca.gerarJogo();
+            
+            while (forca.isRunning()) {
+                System.out.print("Faça seu palpite (uma única letra): ");
+                
+                String entrada = input.next();
+                
+                if (entrada.length() != 1 || !Character.isLetter(entrada.charAt(0))) {
+                    System.out.println("Entrada inválida! Digite apenas uma letra.");
+                    continue;
+                }
+                
+                char palpite = entrada.toLowerCase().charAt(0);
+                forca.adivinharLetra(palpite);
+            }
+            
+            System.out.print("\nDeseja jogar novamente? (s/n): ");
+            jogarNovamente = input.next().equalsIgnoreCase("s");
+            
+        } while (jogarNovamente);
+        
+        System.out.println("Obrigado por jogar! Até a próxima.");
+        input.close();
+    }
+}
