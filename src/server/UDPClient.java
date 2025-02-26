82a2eef9d38b50de773a5ecad02a18b1ec8e7f32
Samara Porto <sp.samaraporto@gmail.com>
2025-02-26 13:57:56 -0300

    feat: informa quem ganhou e correção de bug de entrada ready e restart inválida

--- src/server/UDPClient.java
+++ src/server/UDPClient.java
@@ -20,7 +20,7 @@ public class UDPClient {
         System.out.println("[" + dtf.format(LocalDateTime.now()) + "] "
                 + "Conectando ao servidor...");
 
-        // envia uma mensagem de registro (JOIN) para o servidor, poderia ser um prompt caso não queiramos que o jogador ja entre diretamente
+        // envia uma mensagem de registro JOIN para o servidor, poderia ser um prompt caso não queiramos que o jogador ja entre diretamente
         String joinMessage = "JOIN";
         byte[] sendData = joinMessage.getBytes();
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
@@ -28,41 +28,51 @@ public class UDPClient {
         
         boolean ready = false;
 
-        // loop pra receber mensagens do servidor
+        //loop pra receber mensagens do servidor
         while (true) {
             
                 byte[] receiveData = new byte[1024];
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-                // recebe a resposta do server
+                //recebe a resposta do server
                 clientSocket.receive(receivePacket);
 
-                // processa a msg recebida
+                //processa a msg recebida
                 String serverMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
                 System.out.println("[" + dtf.format(LocalDateTime.now()) + "] "
                         + "FROM SERVER: " + serverMessage);
 
-                // verifica coisas que o jogador tem que realizar
+                //verifica coisas que o jogador tem que realizar
                 if (serverMessage.contains("READY") && !ready) {
-                    System.out.println("Digite 'READY' para confirmar que está pronto:");
-                    String readyInput = keyboardReader.readLine();
-                    if (readyInput.equalsIgnoreCase("READY")) {
-                        sendMessage("READY", clientSocket, serverAddress, serverPort);
-                        ready = true;
+                    while (true) { //fica em loop ate que o jogador digite READY
+                        System.out.println("Digite 'READY' para confirmar que está pronto:");
+                        String readyInput = keyboardReader.readLine();
+                        if (readyInput.equalsIgnoreCase("READY")) {
+                            sendMessage("READY", clientSocket, serverAddress, serverPort);
+                            ready = true;
+                            break; 
+                        } else {
+                            System.out.println("Entrada inválida. Por favor, digite 'READY' para confirmar.");
+                        }
                     }
+                	
                 } else if (serverMessage.startsWith("SUA VEZ")) {
                     System.out.println("[" + dtf.format(LocalDateTime.now()) + "] Digite uma letra para adivinhar:");
                     String guess = keyboardReader.readLine();
                     sendMessage("GUESS " + guess, clientSocket, serverAddress, serverPort);
                 }	else if (serverMessage.contains("Se deseja jogar novamente")) {
-                    
-                    System.out.println("Digite 'RESTART' se deseja reiniciar o jogo:");
-                    String restartInput = keyboardReader.readLine();
-                    if (restartInput.equalsIgnoreCase("RESTART")) {
-                        sendMessage("RESTART", clientSocket, serverAddress, serverPort);
+                    while (true) { 
+                        System.out.println("Digite 'RESTART' se deseja reiniciar o jogo:");
+                        String restartInput = keyboardReader.readLine();
+                        if (restartInput.equalsIgnoreCase("RESTART")) {
+                            sendMessage("RESTART", clientSocket, serverAddress, serverPort);
+                            break; 
+                        } else {
+                            System.out.println("Entrada inválida. Por favor, digite 'RESTART' para reiniciar o jogo.");
+                        }
                     }
-                }else if (serverMessage.contains("Iniciando novo jogo")) {
-                    // Recebe a mensagem de reinício do servidor
+                } else if (serverMessage.contains("Iniciando novo jogo")) {
+                    //recebe a mensagem de reinício do servidor
                     System.out.println("[" + dtf.format(LocalDateTime.now()) + "] "
                             + "O jogo foi reiniciado. Prepare-se para começar novamente!");
                     ready = false;  
